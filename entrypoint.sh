0e92901fa07e069cdffad0dfba632396f38ab819
Rohan Mars <code@rohanmars.com>
2024-03-16 12:07:17 -0700

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ if [ ! -z $INPUT_USERNAME ];
 then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
 fi
 
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v ${{ github.workspace }}:/work --entrypoint=bash $INPUT_IMAGE -c "${cd /work; tiecd INPUT_ARGS//$'\n'/;}"
+echo $INPUT_WORKSPACE
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $INPUT_WORKSPACE:/work --entrypoint=bash $INPUT_IMAGE -c "${cd /work; tiecd INPUT_ARGS//$'\n'/;}"
