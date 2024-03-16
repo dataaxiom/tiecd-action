25e785033e6e6f63d4ccfb29726c3188f5b4ffac
Rohan Mars <code@rohanmars.com>
2024-03-16 15:16:56 -0700

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,8 @@ if [ ! -z $INPUT_DOCKER_NETWORK ];
 then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 
-set
-echo $INPUT_OPTIONS
-echo $TIECD_WORKSPACE
-echo $INPUT_RUN
+echo "rophan"
+echo $INPUT_WORKSPACE
+echo "mars"
 IMAGE=ghcr.io/dataaxiom/tiecd:$INPUT_IMAGE
-exec docker run --rm -v "/var/run/docker.sock":"/var/run/docker.sock" -v $INPUT_WORKSPACE:/scratch --entrypoint=bash $IMAGE -c "cd /scratch; ${INPUT_RUN//$'\n'/;}"
+exec docker run --rm -v "/var/run/docker.sock":"/var/run/docker.sock" -v $INPUT_WORKSPACE:/scratch --entrypoint=bash $IMAGE -c "cd /scratch; ls -al; ${INPUT_RUN//$'\n'/;}"
