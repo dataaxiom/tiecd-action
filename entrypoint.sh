33ab593aec27bbb08cde7b69403895e25870be05
Rohan Mars <code@rohanmars.com>
2024-03-16 14:00:47 -0700

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ if [ ! -z $INPUT_USERNAME ];
 then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
 fi
 
-echo $INPUT_WORKSPACE
+if [ ! -z $INPUT_DOCKER_NETWORK ];
+then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
+fi
 
-#exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=bash $INPUT_IMAGE -c "cd /work; tiecd ${INPUT_ARGS//$'\n'/;}"
-#exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $INPUT_WORKSPACE:/work --entrypoint=bash $INPUT_IMAGE -c "cd /work; tiecd ${INPUT_ARGS//$'\n'/;}"
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
