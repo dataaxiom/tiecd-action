93c4b362921dcb407ad6a3df66a25ab655e1ad5b
Rohan Mars <code@rohanmars.com>
2024-03-16 12:13:25 -0700

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,6 @@ then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --pa
 fi
 
 echo $INPUT_WORKSPACE
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $INPUT_WORKSPACE:/work --entrypoint=bash $INPUT_IMAGE -c "${cd /work; tiecd ${INPUT_ARGS//$'\n'/;}"
+
+#exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $INPUT_WORKSPACE:/work --entrypoint=bash $INPUT_IMAGE -c "cd /work; tiecd ${INPUT_ARGS//$'\n'/;}"
