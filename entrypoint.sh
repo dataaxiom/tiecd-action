222c6452e88d604c4672d72f849b0d37eec70847
Rohan Mars <code@rohanmars.com>
2024-03-16 11:58:31 -0700

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 
-#if [ ! -z $INPUT_USERNAME ];
-#then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
-#fi
-#
-#if [ ! -z $INPUT_DOCKER_NETWORK ];
-#then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
-#fi
+if [ ! -z $INPUT_USERNAME ];
+then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
+fi
 
-ls -al /github
-INPUT_RUN="pwd; ls -al /scratch; tiecd deploy"
-echo Using image: $INPUT_VERSION
-mkdir /scratch
-cp -r /github/workspace/* /scratch
-ls -al /scratch
-exec docker run --rm -v "/var/run/docker.sock":"/var/run/docker.sock" -v /scratch:/scratch:rw --workdir=/scratch  --entrypoint=bash ghcr.io/dataaxiom/tiecd:$INPUT_VERSION -c "${INPUT_RUN//$'\n'/;}"
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v ${{ github.workspace }}:/work --entrypoint=bash $INPUT_IMAGE -c "${cd /work; tiecd INPUT_ARGS//$'\n'/;}"
--- tiecd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker pull ghcr.io/dataaxiom/tiecd:main-k8s
-docker run --rm ghcr.io/dataaxiom/tiecd:main-k8s tiecd --help
